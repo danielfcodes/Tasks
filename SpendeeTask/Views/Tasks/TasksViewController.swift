1901e9ea037bf5696d37e46aa39246c8d84f9de8
Daniel Fernandez <dyopla@techona.com>
2019-10-21 22:26:21 +0200

    Initial commit

---
+++ SpendeeTask/Views/Tasks/TasksViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TasksViewController.swift
+//  SpendeeTask
+//
+//  Created by Daniel Fernandez on 10/21/19.
+//  Copyright © 2019 danielfcodes. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialSetup()
+    }
+    
+    private func initialSetup() {
+        view.backgroundColor = .white
+        title = "Tasks"
+    }
+    
+}
